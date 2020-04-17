30bdbf9917a89a43e5e2a2f4c7e85ad6f5dcd52e
robyafrizal <robyafrizal86@gmail.com>
2020-04-17 23:28:38 +0700

    Updates

---
+++ precache-manifest.b783c2ba912ce9b116fc1565b6b1d93b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b96120087ad17380c36b6ece2f270e",
+    "url": "/react-tasklist/index.html"
+  },
+  {
+    "revision": "c45730c58ecf653ab53e",
+    "url": "/react-tasklist/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "7b9cf1f8f4c73fa7408a",
+    "url": "/react-tasklist/static/css/main.17c246c0.chunk.css"
+  },
+  {
+    "revision": "c45730c58ecf653ab53e",
+    "url": "/react-tasklist/static/js/2.a3111d8a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-tasklist/static/js/2.a3111d8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b9cf1f8f4c73fa7408a",
+    "url": "/react-tasklist/static/js/main.d00cb441.chunk.js"
+  },
+  {
+    "revision": "81fcab5e97736ae09d34",
+    "url": "/react-tasklist/static/js/runtime-main.46bf40f9.js"
+  }
+]);
